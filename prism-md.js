d3731a8aa15dc21c6e5d929741dca7237795fe15
JT <jt@gosquared.com>
2015-04-01 18:01:27 +0100

    Many things

--- prism-md.js
+++ prism-md.js
@@ -14,87 +14,91 @@ Prism.languages.md = (function(){
     }
   }
 
+  var inlines = {};
+  var blocks = {};
+
+  function inline(name, def){
+    blocks[name] = inlines[name] = md[name] = def;
+  }
+  function block(name, def){
+    blocks[name] = md[name] = def;
+  }
+  function leafBlock(name, def){
+    md[name] = def;
+  }
 
 
-  md['heading setext-heading heading-1'] = {
+  leafBlock('code-block fenced', {
+    pattern: /(^ {0,3}|\n {0,3})(`{3,})[^`\n]*\n(?:[\s\S]*?)\n {0,3}(\2`*(?= *\n)|$)/g,
+    lookbehind: true,
+    inside: {
+      'code-language': {
+        pattern: /(^`+)[^`\n]+/,
+        lookbehind: true
+      },
+      'marker code-fence start': /^`+/,
+      'marker code-fence end': /`+$/,
+      'code-inner': {
+        pattern: /(^\n)[\s\S]*(?=\n$)/,
+        lookbehind: true
+      }
+    }
+  });
+
+
+  leafBlock('heading setext-heading heading-1', {
     pattern: /^ {0,3}[^\s].*\n {0,3}=+[ \t]*$/gm,
     inside: {
       'marker heading-setext-line': {
         pattern: /^( {0,3}[^\s].*\n) {0,3}=+[ \t]*$/gm,
         lookbehind: true
-      }
+      },
+      rest: inlines
     }
-  };
+  });
 
-  md['heading setext-heading heading-2'] = {
+  leafBlock('heading setext-heading heading-2', {
     pattern: /^ {0,3}[^\s].*\n {0,3}-+[ \t]*$/gm,
     inside: {
       'marker heading-setext-line': {
         pattern: /^( {0,3}[^\s].*\n) {0,3}-+[ \t]*$/gm,
         lookbehind: true
-      }
+      },
+      rest: inlines
     }
-  };
+  });
 
   var headingInside = {
     'marker heading-hash start': /^ *#+ */,
-    'marker heading-hash end': / +#+ *$/
+    'marker heading-hash end': / +#+ *$/,
+    rest: inlines
   };
   for(var i = 1; i <= 6; i += 1){
-    md['heading heading-'+i] = {
+    leafBlock('heading heading-'+i, {
       pattern: new RegExp('^ {0,3}#{'+i+'}(?!#).*$', 'gm'),
       inside: headingInside
-    };
+    });
   }
 
 
-  md.hr = {
-    pattern: /^[\t ]*([*\-_])([\t ]*\1){2,}([\t ]*$)/gm,
-    inside: {
-      'marker hr-marker': /[*\-_]/g
-    }
-  };
-
-  md.blockquote = {
-    pattern: /^[\t ]*>[\t ]?.+(?:\n(?!\n)|.)*/gm,
-    inside: {
-      'marker quote-marker': /^[\t ]*>[\t ]?/gm,
-      'blockquote-content': {
-        pattern: /[^>]+/
-      }
-    }
-  };
-
-  md.list = {
-    pattern: /^[\t ]*([*+\-]|\d+\.)[\t ].+(?:\n(?!\n)|.)*/gm,
-    inside: {
-      li: {
-        pattern: /^[\t ]*([*+\-]|\d+\.)[\t ].+(?:\n|[ \t]+[^*+\- \t].*\n)*/gm,
-        inside: {
-          'marker list-item': /^[ \t]*([*+\-]|\d+\.)[ \t]/m
-        }
-      }
-    }
-  };
-
-
-  md.code = {
-    pattern: /(^|[^\\])(`+)([^\r]*?[^`])\2(?!`)/g,
-    lookbehind: true,
-    inside: {
-      'code-inner': {
-        pattern: /^(`)(.|\s)*(?=`$)/,
-        lookbehind: true
-      },
-      'marker code-marker start': /^`/,
-      'marker code-marker end': /`$/
-    }
-  };
 
   var linkText = {
     pattern: /^\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]/,
     inside: {
       'link-text-inner': {
+        pattern: /^(\[)(.|\s)*?(?=\]$)/,
+        lookbehind: true,
+        inside: inlines
+      },
+      'marker bracket start': /^\[/,
+      'marker bracket end': /\]$/
+    }
+  };
+
+  var linkLabel = {
+    pattern: /\[(?:\\.|[^\]])*\]/,
+    inside: {
+      'link-label-inner': {
         pattern: /^(\[)(.|\s)*?(?=\]$)/,
         lookbehind: true
       },
@@ -109,7 +113,8 @@ Prism.languages.md = (function(){
       'marker image-bang': /^!/,
       'image-text-inner': {
         pattern: /^(\[)(.|\s)*?(?=\]$)/,
-        lookbehind: true
+        lookbehind: true,
+        inside: inlines
       },
       'marker bracket start': /^\[/,
       'marker bracket end': /\]$/
@@ -117,12 +122,12 @@ Prism.languages.md = (function(){
   };
 
   var linkURL = {
-    pattern: /^(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))+/,
-    // lookbehind: true
+    pattern: /^(\s*)(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))+/,
+    lookbehind: true
   };
 
   var linkBracedURL = {
-    pattern: /^(\s*)<(?:\\.|[^<>])*>/,
+    pattern: /^(\s*)<(?:\\.|[^<>\n])*>/,
     lookbehind: true,
     inside: {
       'braced-href-inner': {
@@ -135,8 +140,8 @@ Prism.languages.md = (function(){
   };
 
   var linkTitle = {
-    pattern: /(\s+)('(?:\\'|[^'])+'|"(?:\\"|[^"])+")/,
-    lookbehind: true,
+    pattern: /('(?:\\'|[^'])+'|"(?:\\"|[^"])+")\s*$/,
+    // lookbehind: true,
     inside: {
       'title-inner': {
         pattern: /^(['"])(.|\s)*?(?=\1$)/,
@@ -148,15 +153,15 @@ Prism.languages.md = (function(){
   };
 
   var linkParams = {
-    pattern: /\(\s*(?:(?!<)(?:\\.|[^\(\)]|\([^\(\)]*\))*|<(?:[^<>]|\\.)*>)(\s+('(?:[^']|\\')+'|"(?:[^"]|\\")+"))?\s*\)/,
+    pattern: /\( *(?:(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))*|<(?:[^<>\n]|\\.)*>)( +('(?:[^']|\\')+'|"(?:[^"]|\\")+"))? *\)/,
     inside: {
       'link-params-inner': {
         pattern: /^(\(\s*)(.|\n)*?(?=\s*\)$)/,
         lookbehind: true,
         inside: {
+          'link-title': linkTitle,
           'href': linkURL,
           'braced-href': linkBracedURL,
-          'link-title': linkTitle
         }
       },
       'marker bracket start': /^\(/,
@@ -164,63 +169,159 @@ Prism.languages.md = (function(){
     }
   };
 
-  md.image = {
-    pattern: /!\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]\(\s*(?:(?!<)(?:\\.|[^\(\)]|\([^\(\)]*\))*|<(?:[^<>]|\\.)*>)(\s+('(?:[^']|\\')+'|"(?:[^"]|\\")+"))?\s*\)/,
+
+
+
+  leafBlock('hr', {
+    pattern: /^[\t ]*([*\-_])([\t ]*\1){2,}([\t ]*$)/gm,
+    inside: {
+      'marker hr-marker': /[*\-_]/g
+    }
+  });
+
+  leafBlock('urldef', {
+    pattern: /^( {0,3})\[(?:\\.|[^\]])+]: *\n? *(?:(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))*|<(?:[^<>\n]|\\.)*>)( *\n? *('(?:\\'|[^'])+'|"(?:\\"|[^"])+"))?$/gm,
+    lookbehind: true,
+    inside: {
+      'link-label': linkLabel,
+      'marker urldef-colon': /^:/,
+      'link-title': linkTitle,
+      'href': linkURL,
+      'braced-href': linkBracedURL,
+    }
+  });
+
+  block('blockquote', {
+    pattern: /^[\t ]*>[\t ]?.+(?:\n(?!\n)|.)*/gm,
+    inside: {
+      'marker quote-marker': /^[\t ]*>[\t ]?/gm,
+      'blockquote-content': {
+        pattern: /[^>]+/,
+        rest: blocks
+      }
+    }
+  });
+
+  block('list', {
+    pattern: /^[\t ]*([*+\-]|\d+\.)[\t ].+(?:\n(?!\n)|.)*/gm,
+    inside: {
+      li: {
+        pattern: /^[\t ]*([*+\-]|\d+\.)[\t ].+(?:\n|[ \t]+[^*+\- \t].*\n)*/gm,
+        inside: {
+          'marker list-item': /^[ \t]*([*+\-]|\d+\.)[ \t]/m,
+          rest: blocks
+        }
+      }
+    }
+  });
+
+  leafBlock('code-block', {
+    pattern: /(^|(?:^|(?:^|\n)(?![ \t]*([*+\-]|\d+\.)[ \t]).*\n)\s*?\n)((?: {4}|\t).*(?:\n|$))+/g,
+    lookbehind: true
+  });
+
+  inline('image', {
+    pattern: /!\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]\(\s*(?:(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))*|<(?:[^<>\n]|\\.)*>)(\s+('(?:[^']|\\')+'|"(?:[^"]|\\")+"))?\s*\)/,
     inside: {
       'link-text': imageText,
       'link-params': linkParams
     }
-  };
+  });
 
-  md.link = {
-    pattern: /\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]\(\s*(?:(?!<)(?:\\.|[^\(\)]|\([^\(\)]*\))*|<(?:[^<>]|\\.)*>)(\s+('(?:[^']|\\')+'|"(?:[^"]|\\")+"))?\s*\)/,
+  inline('link', {
+    pattern: /\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]\(\s*(?:(?!<)(?:\\.|[^\(\)\s]|\([^\(\)\s]*\))*|<(?:[^<>\n]|\\.)*>)(\s+('(?:[^']|\\')+'|"(?:[^"]|\\")+"))?\s*\)/,
     inside: {
       'link-text': linkText,
       'link-params': linkParams
     }
-  };
+  });
+
+  inline('image image-ref', {
+    pattern: /!\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\] ?\[(?:\\.|[^\]])*\]/,
+    inside: {
+      'link-text': imageText,
+      'link-label': linkLabel
+    }
+  });
+  inline('link link-ref', {
+    pattern: /\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\] ?\[(?:\\.|[^\]])*\]/,
+    inside: {
+      'link-text': linkText,
+      'link-label': linkLabel
+    }
+  });
+
+  inline('image image-ref shortcut-ref', {
+    pattern: /!\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]/,
+    inside: {
+      'marker image-bang': /^!/,
+      'link-text': linkText
+    }
+  });
+  inline('link link-ref shortcut-ref', {
+    pattern: /\[(?:\\.|[^\[\]]|\[[^\[\]]*\])*\]/,
+    inside: {
+      'link-text': linkText
+    }
+  });
 
 
-  md.strong = {
+  inline('code', {
+    pattern: /(^|[^\\])(`+)([^\r]*?[^`])\2(?!`)/g,
+    lookbehind: true,
+    inside: {
+      'code-inner': {
+        pattern: /^(`)(.|\s)*(?=`$)/,
+        lookbehind: true
+      },
+      'marker code-marker start': /^`/,
+      'marker code-marker end': /`$/
+    }
+  });
+
+  inline('strong', {
     pattern: /(^|[^\\*_])([_\*])\2(?:\n(?!\n)|.)+?\2{2}(?!\2)/g,
     // pattern: /(^|[^\\])(\*\*|__)(?:\n(?!\n)|.)+?\2/,
     lookbehind: true,
     inside: {
       'strong-inner': {
         pattern: /^(\*\*|__)(.|\s)*?(?=\1$)/,
-        lookbehind: true
+        lookbehind: true,
+        inside: inlines
       },
       'marker strong-marker start': /^(\*\*|__)/,
       'marker strong-marker end': /(\*\*|__)$/
     }
-  };
+  });
 
-  md.em = {
+  inline('em', {
     // pattern: /(^|[^\\])(\*|_)(\S[^\2]*?)??[^\s\\]+?\2/g,
     pattern: /(^|[^\\*_])(\*|_)(?:\n(?!\n)|.)+?\2(?!\2)/g,
 		lookbehind: true,
     inside: {
       'em-inner': {
         pattern: /^(\*|_)(.|\s)*?(?=\1$)/,
-        lookbehind: true
+        lookbehind: true,
+        inside: inlines
       },
       'marker em-marker start': /^(\*|_)/,
       'marker em-marker end': /(\*|_)$/
     }
-  };
+  });
 
-  md.strike = {
+  inline('strike', {
     pattern: /(^|\n|\W)(~~)(?=\S)([^\r]*?\S)\2/gm,
     lookbehind: true,
     inside: {
       'strike-inner': {
         pattern: /^(~~)(.|\s)*(?=~~$)/,
-        lookbehind: true
+        lookbehind: true,
+        inside: inlines
       },
       'marker strike-marker start': /^~~/,
       'marker strike-marker end': /~~$/
     }
-  };
+  });
 
   // md.nl = /^\n$/gm;
 
@@ -238,9 +339,11 @@ Prism.languages.md = (function(){
     strike: md.strike,
     code: md.code,
     link: md.link,
-    image: md.image
+    image: md.image,
+    'image image-ref': md['image image-ref'],
+    'link link-ref': md['link link-ref']
   };
-
+/*
   var blocks = {
     list: md.list,
     blockquote: md.blockquote
@@ -248,7 +351,7 @@ Prism.languages.md = (function(){
   merge(blocks, inlines);
 
   addInside(md.blockquote.inside['blockquote-content'], blocks);
-  addInside(md.list.inside.li/*.inside['list-item-inner']*/, blocks);
+  addInside(md.list.inside.li, blocks);
 
   for(var i = 1; i <= 6; i += 1){
     addInside(md['heading heading-'+i], inlines);
@@ -261,10 +364,12 @@ Prism.languages.md = (function(){
 
   var linkInlines = shallowClone(inlines);
   delete linkInlines.link;
+  delete linkInlines['link link-ref'];
   addInside(linkText.inside['link-text-inner'], linkInlines);
 
   var imgInlines = shallowClone(linkInlines);
   delete imgInlines.image;
+  delete imgInlines['image image-ref'];
   addInside(imageText.inside['image-text-inner'], imgInlines);
 
 
@@ -278,7 +383,7 @@ Prism.languages.md = (function(){
   md.strong.inside['strong-inner'].inside.em = md.em;
   md.em.inside['em-inner'].inside.strong = md.strong;
 
-
+*/
 
   return md;
 })();
