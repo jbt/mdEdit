adbe3ca03420c15de9db21398b95b65770af64f0
Hendrik Leppelsack <hendrik@leppelsack.de>
2015-10-02 15:17:37 +0200

    add mdEdit class instead of replacing existing classes

--- src/Editor.js
+++ src/Editor.js
@@ -15,7 +15,8 @@ function Editor(el, opts){
 
   var cname = opts['className'] || '';
 
-  this.el.className = 'mdedit' + (cname ? ' ' + cname : '');
+  this.el.className = this.el.className ? this.el.className + ' ' : '';
+  this.el.className += 'mdedit' + (cname ? ' ' + cname : '');
   this.el.setAttribute('contenteditable', true);
 
   this.selMgr = new SelectionManager(el);
